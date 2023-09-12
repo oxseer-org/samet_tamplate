1a9fe79701f676ee0009d9c2d697af8ac3d47f7b
Samet <samettaspinarx@gmail.com>
2023-09-12 17:04:29 -0400

    Update conftest.py

--- tests_unit/conftest.py
+++ tests_unit/conftest.py
@@ -4,7 +4,6 @@ References:
 - https://stackoverflow.com/questions/44377358/how-can-i-display-the-test-name-after-the-test-using-pytest
 - https://docs.pytest.org/en/stable/fixture.html
 """
-import pathlib
 
 import pytest
 
@@ -13,11 +12,9 @@ import pytest
 def env_setup(monkeypatch):
     """Setup Environment Variables through PyTest's `monkeypatch` feature.
 
-    TODO (tommy@overjet.ai): Configure this for your repository. See an example below of how to use
-        the `pytest` "`monkeypatch`" feature -- postfixes.
     """
     # monkeypatch.setenv(
     #     "GCLOUD_PROJECT",
-    #     "cariesdetection",
+    #     "samet-project",
     # )
     return None
